7c408b2e26196d1165d4e63c760c7022c275d669
Dmitrii Piniaskin <pinyaskin@gran-call.ru>
2024-11-14 22:40:11 +0100

    init

---
+++ src/main/java/com/pocketm/TrialTaskCommand.java
@@ -0,0 +1,22 @@
+package com.pocketm;
+
+import com.pocketm.service.converter.FileConverterService;
+import io.micronaut.configuration.picocli.PicocliRunner;
+
+import jakarta.inject.Inject;
+import picocli.CommandLine.Command;
+
+@Command(name = "trial-task", description = "Trial Task. Dmitrii Piniaskin", mixinStandardHelpOptions = true)
+public class TrialTaskCommand implements Runnable {
+
+    @Inject
+    private FileConverterService fileConverterService;
+
+    public static void main(String[] args) {
+        PicocliRunner.run(TrialTaskCommand.class, args);
+    }
+
+    public void run() {
+        fileConverterService.convert();
+    }
+}
