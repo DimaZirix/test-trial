8c2bb5e45ed667f2eac67149ac3f26eb5232b7e3
Dmitrii Piniaskin <pinyaskin@gran-call.ru>
2024-11-18 13:25:28 +0100

    Add the export jsons

---
+++ src/main/java/com/pocketm/domain/json/RootJson.java
@@ -0,0 +1,15 @@
+package com.pocketm.domain.json;
+
+import lombok.*;
+
+@ToString
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RootJson {
+
+    private ContentJson content;
+
+}
